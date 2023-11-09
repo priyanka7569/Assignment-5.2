17966fe7ff1da87cf62cd1725858384238e5c5fc
priyanka7569 <kota.priyanka@mphasis.com>
2023-11-09 15:55:04 +0530

    this is my 5.2 program

---
+++ Assignment 5.2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_5._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Part 3: Fibonacci sequence
+            Console.WriteLine("\nPart 3: Fibonacci Sequence");
+
+            int n = 10;
+            int a = 0, b = 1;
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(a + " ");
+                int temp = a;
+                a = b;
+                b = temp + b;
+            }
+            Console.ReadKey();
+        }
+    }
+}
